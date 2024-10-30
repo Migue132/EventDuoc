f33c0465eaa2af646963ee05a370ab1f36d4cfe7
migue <91097986+Migue132@users.noreply.github.com>
2024-10-29 22:12:12 -0300

    Add files via upload

---
+++ Calendar.js
@@ -0,0 +1,9 @@
+[   { 
+        "titulo": "Noticia 1", 
+        "descripcion": "Descripción de la noticia 1",  
+    }, 
+    { 
+        "titulo": "Noticia 2", 
+        "descripcion": "Descripción de la noticia 2", 
+    } 
+]
